c595a68de316409a694844ecacd07e4eaefb954a
Irina 13 <LIA130594@yandex.ru>
2025-03-22 17:01:08 +0500

    sort

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,4 +1,9 @@
 // TODO: write your code here
+import sum from './basic';
+ 
+console.log('worked');
+ 
+console.log(sum([1, 2]));
 
 
 import showHealth from './health';
@@ -6,3 +11,13 @@ import showHealth from './health';
 console.log('worked');
 
 showHealth(70);
+
+
+import sortUnits from './sort';
+
+const a = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+sortUnits(a);
